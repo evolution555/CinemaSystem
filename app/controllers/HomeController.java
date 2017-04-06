527bc3b6292703d594038d178938a2053c9ca1b4
Evan Conlon <evan.conlon.555@gmail.com>
2017-04-06 16:15:39 +0100

    Revert "Revert "Search function has been added""

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -39,8 +39,14 @@ public class HomeController extends Controller {
         return ok(index.render(u, allFilms, env, allCarousel));
     }
 
-    public Result aboutUs(){
-        return ok(contactUs.render(getUserFromSession()));
+
+    public Result search() {
+        DynamicForm searchForm = formFactory.form().bindFromRequest();
+        String filmTitle = searchForm.get("title");
+        User u = getUserFromSession();
+        List<Film> searchFilms = Film.search(filmTitle);
+        List<carousel> allCarousel = carousel.findAll();
+        return ok(index.render(u, searchFilms, env, allCarousel));
     }
 
     public Result film() {
